5c8afe14054851eb58ab020d0dec0039b1ab8930
Kayleigh Pilgrim <k@abs.gd>
2025-03-08 14:40:13 +0100

    Move layout to seperate component

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,9 +1,6 @@
-"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { logout } from "../utils/api";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Navbar from "../components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,34 +12,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-/*export*/ const metadata = {
+export const metadata = {
   title: "ABS NOC",
   description: "The ABS Network Operation Center.",
 };
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname(); // Get current page
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex flex-col min-h-screen">
-          <header className="bg-gray-800 text-white p-4 flex justify-between">
-            <h1 className="text-xl font-bold">ABS NOC</h1>
-            <nav className="flex gap-4">
-              <Link href="/" className={pathname === "/" ? "font-bold" : ""}>
-                Home
-              </Link>
-              <Link href="/monitoring" className={pathname === "/monitoring" ? "font-bold" : ""}>
-                Monitoring
-              </Link>
-            </nav>
-            <button onClick={logout} className="bg-red-500 px-4 py-2 rounded">
-              Logout
-            </button>
-          </header>
+          <Navbar />
           <main className="flex-grow p-4">{children}</main>
         </div>
       </body>
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,18 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+      return;
+    }
+  }, []);
+  
   return (
     <h2 className="text-2xl font-bold mb-4">Welcome!</h2>
   );
