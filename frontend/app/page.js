2c8d98fff67606b32f6f5ccf1774b9bf2f98ab79
Kayleigh Pilgrim <k@abs.gd>
2025-03-08 14:30:44 +0100

    Cleaning up commented code

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,26 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { getServers } from "../utils/api";
-import ServerCard from "../components/ServerCard";
-
 export default function Dashboard() {
-  /*const [servers, setServers] = useState([]);
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.push("/login");
-      return;
-    }
-
-    getServers(token)
-      .then((res) => setServers(res.data))
-      .catch(() => router.push("/login"));
-  }, []);*/
-
   return (
     <h2 className="text-2xl font-bold mb-4">Welcome!</h2>
   );
