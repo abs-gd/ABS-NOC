5c20916f5d4436ff73404c85229a9301bdf397ba
Kayleigh Pilgrim <k@abs.gd>
2025-03-11 13:24:47 +0100

    Seperated cors

--- public/index.php
+++ public/index.php
@@ -1,41 +1,14 @@
 <?php
-// Remove any existing headers that might interfere
-header_remove("Access-Control-Allow-Origin");
-header_remove("Access-Control-Allow-Methods");
-header_remove("Access-Control-Allow-Headers");
+use App\Middleware\Cors;
+use App\Controllers\HomeController;
+use App\Controllers\AuthController;
 
-// Set CORS headers manually
-$allowed_origin = "http://noc.abs.test:80";
-header("Access-Control-Allow-Origin: $allowed_origin");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PATCH, DELETE, PUT");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// FOR LOCAL TESTING
-/*header("Content-Security-Policy: default-src 'self' 'unsafe-inline' 'unsafe-eval' http: https: data: blob:;");*/
-
-// Handle preflight (OPTIONS request)
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-    http_response_code(204);
-    exit();
-}
-
-session_set_cookie_params([
-    //'lifetime' => 0,            // Session expires when the browser closes
-    //'path' => '/',
-    //'domain' => '',             // Set if using a specific domain
-    'secure' => false,           // Doesn't require HTTPS (change in production)
-    'httponly' => true,         // Prevents JavaScript access to the cookie
-    //'samesite' => 'Lax'        // Allows cross-site cookies
-    'samesite' => 'Strict'        // Allows cross-site cookies
-]);
+Cors::handle(); // Apply CORS handling
 
 session_start();
 //var_dump($_SESSION);
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use App\Controllers\HomeController;
-use App\Controllers\AuthController;
 
 $routes = require __DIR__ . '/../routes/web.php';
 
@@ -43,7 +16,6 @@ $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (isset($routes[$requestUri])) {
     [$controller, $method] = explode('@', $routes[$requestUri]);
     
-    // Dynamically call the controller with namespace
     $controllerClass = "App\\Controllers\\$controller";
     
     if (class_exists($controllerClass)) {
